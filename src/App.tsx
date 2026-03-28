4bd168f66a3cdb08edb6eac3df031fe88fab7c9f
bittubdserverid8294-collab <bittubdserverid8294@gmail.com>
2026-03-28 19:03:36 +0530

    feat: Update package name and SDK versions
    Refactors the application to use `com.rootlib.hub` as the package name, aligning with the project's underlying structure. Also updates the `minSdkVersion` to 21 and `targetSdkVersion` to 33 in the Android manifest and build.gradle file to ensure compatibility with modern Android versions and leverage newer platform features.

--- src/App.tsx
+++ src/App.tsx
@@ -984,6 +984,8 @@ public class WebAppInterface {
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     package="com.rootlib.hub">
     
+    <uses-sdk android:minSdkVersion="21" android:targetSdkVersion="33" />
+
     <uses-permission android:name="android.permission.INTERNET" />
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
     <uses-permission android:name="android.permission.ACCESS_SUPERUSER" />
